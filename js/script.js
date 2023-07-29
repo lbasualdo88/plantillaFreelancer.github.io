c0ce54f65e04a8457dca89f9b4d6e8328063ab67
Leonardo Basualdo <lbasualdo88@gmail.com>
2023-07-29 15:21:44 -0300

    Agregando js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const botonContactar = document.querySelector(".botonContactar")
+
+botonContactar.addEventListener('click', function mostrarMensaje(){
+    alert("Funcion no disponible por ahora, contactar por redes, disculpe las molestias");
+})
+
