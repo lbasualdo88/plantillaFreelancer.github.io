0ee054c4ce2a2e5b63ead4cdc6661b920010abd7
Leonardo Basualdo <lbasualdo88@gmail.com>
2023-07-29 15:50:45 -0300

    Actualizacion de script

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ botonContactar.addEventListener('click', function mostrarMensaje(){
     alert("Funcion no disponible por ahora, contactar por redes, disculpe las molestias");
 })
 
+const botonEnviar = document.querySelector(".botonEnviar")
+
+
+botonEnviar.addEventListener('click', function mostrarMSJ(){
+    alert("Estamos validando el formulario para que se pueda contactar, disculpe las molestias, puede encontrarme por linkedln");
+})
